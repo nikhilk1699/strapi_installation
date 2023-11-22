98bb43af829ce6e7391d16e7626529f7824f3278
nikhilk1699 <109533285+nikhilk1699@users.noreply.github.com>
2023-11-22 11:59:23 +0530

    Delete automate_installation/backend.tf

--- automate_installation/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
- backend "azurerm" {
-   resource_group_name = "nstrapi-rg"
-    storage_account_name = "strapistroage"
-    container_name = "secretfile"
-    key = "terraform.tfstate"
-  }
-}
